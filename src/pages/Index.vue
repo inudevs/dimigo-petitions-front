ad2b64dd7c61c19746fdc58ce393e6ff9f8874f0
JunhoYeo <32605822+JunhoYeo@users.noreply.github.com>
2019-07-05 10:49:44 +0900

    feat: menu

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,16 +1,29 @@
 <script>
 import DimiHeader from '@/components/DimiHeader'
+import DimiMenu from '@/components/DimiMenu'
 
 export default {
   name: 'Index',
   components: {
-    DimiHeader
+    DimiHeader,
+    DimiMenu
   }
 }
 </script>
 
 <template>
-  <div class="content">
+  <div class="page">
     <dimi-header />
+    <dimi-menu />
+    <div class="content">
+    </div>
   </div>
 </template>
+
+<style lang="scss" scoped>
+.content {
+  display: flex;
+  justify-content: center;
+  width: 100%;
+}
+</style>
