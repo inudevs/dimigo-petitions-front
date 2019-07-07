42861c2a9d5302237ef5a57cdfc74b98e6ccf2ff
mhibio <mhibio@naver.com>
2019-07-08 08:57:57 +0900

    feat: pages

---
+++ src/pages/Petitions.vue
@@ -0,0 +1,29 @@
+<script>
+import DimiHeader from '@/components/DimiHeader'
+import DimiMenu from '@/components/DimiMenu'
+
+export default {
+  name: 'Petitions',
+  components: {
+    DimiHeader,
+    DimiMenu
+  }
+}
+</script>
+
+<template>
+  <div class="page">
+    <dimi-header />
+    <dimi-menu />
+    <div class="content">
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.content {
+  display: flex;
+  justify-content: center;
+  width: 100%;
+}
+</style>
