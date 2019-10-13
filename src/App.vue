3b94fbf70e250defd850055b73ac2c8434122b9a
JunhoYeo <32605822+JunhoYeo@users.noreply.github.com>
2019-10-14 03:09:55 +0900

    refactor: login page again

--- src/App.vue
+++ src/App.vue
@@ -12,9 +12,10 @@ export default {
 
 <style lang="scss">
 @import url('https://fonts.googleapis.com/css?family=Noto+Sans+KR:300,400,500,700,900&display=swap');
+@import url('https://fonts.googleapis.com/css?family=Montserrat:300,400,500,700,900&display=swap');
 
 * {
-  font-family: 'Noto Sans KR', sans-serif;
+  font-family: 'Montserrat', 'Noto Sans KR', sans-serif;
 }
 
 .page {
--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -9,6 +9,7 @@ export default {
   },
   data () {
     return {
+      enter: false,
       form: {
         email: '',
         password: ''
@@ -16,7 +17,7 @@ export default {
     }
   },
   methods: {
-    async onClick () {
+    async onClickLogin () {
       try {
         console.log(this.form)
         const { data } = await this.$api.post('http://api.inudevs.com/auth/login', this.form)
@@ -32,237 +33,339 @@ export default {
 </script>
 
 <template>
-  <div class="page">
-    <div class="content">
-      <div class="card-wrap">
-        <div class="card">
-          <div class="login">
-            <h1 class="login__title">
-              <span class="login__brand">디미청원</span>에 로그인
-            </h1>
-            <p class="login__desc">
-              디미청원은 한국디지털미디어고등학교 학생회와 자율동아리 INU가 함께 운영하는 <strong>교내 학생 건의함</strong> 서비스입니다.
-            </p>
-            <ul class="login__notice">
-              <li>청원에 동의하려면 <strong>로그인</strong>이 필요합니다. 디미고 계정의 아이디 또는 이메일로 로그인해 주세요.</li>
-              <li>디미고 아이디를 분실하신 경우, 교내 인트라넷 관리자나 선생님께 문의하세요.</li>
-            </ul>
-            <div class="login__form">
-              <input
-                class="login__input"
-                v-model.trim="form.email"
-                placeholder="디미고 아이디"
-                @keydown.enter="onClick"
-              />
-              <input
-                type="password"
-                class="login__input"
-                v-model.trim="form.password"
-                placeholder="패스워드"
-                @keydown.enter="onClick"
-              />
-              <div
+  <div class="login">
+    <div class="login__content">
+      <section class="login__image-wrap">
+        <img
+          class="login__image"
+          :src="require('../assets/login-cover.jpg')"
+        >
+      </section>
+      <section class="login__section">
+        <div class="login__section-content">
+          <div class="login__main">
+            <div
+              v-if="!enter"
+              class="login__info"
+            >
+              <h1 class="login__title">
+                디미청원
+              </h1>
+              <p class="login__desc">
+                한국디지털미디어고등학교 교내 건의함 서비스
+              </p>
+              <button
                 class="login__button"
-                @click="onClick"
+                @click="enter = true"
               >
-                동의하러 가기
+                ENTER
+              </button>
+            </div>
+            <div
+              v-else
+              class="login__form"
+            >
+              <div class="login__form-content">
+                <input
+                  v-model="form.email"
+                  placeholder="디미고 아이디"
+                >
+                <input
+                  v-model="form.password"
+                  placeholder="패스워드"
+                  type="password"
+                >
+              </div>
+              <button
+                class="login__button"
+                @click="onClickLogin"
+              >
+                LOGIN
+              </button>
+            </div>
+          </div>
+          <div class="login__cards">
+            <div class="login__card">
+              <img :src="require('../assets/hakbong.jpeg')">
+              <div class="login__card-content">
+                <h1>매점 운영 시간을 연장해 주세요</h1>
+                <span>
+                  <span class="value">300</span>명
+                </span>
+              </div>
+            </div>
+            <div class="login__card">
+              <img :src="require('../assets/hakbong.jpeg')">
+              <div class="login__card-content">
+                <h1>야외 점호를 폐지합시다</h1>
+                <span>
+                  <span class="value">300</span>명
+                </span>
+              </div>
+            </div>
+            <div class="login__card">
+              <img :src="require('../assets/hakbong.jpeg')">
+              <div class="login__card-content">
+                <h1>인강실 사용 제한을 없애주세요</h1>
+                <span>
+                  <span class="value">300</span>명
+                </span>
               </div>
             </div>
           </div>
-          <img
-            class="image"
-            :src="require('../assets/login-cover.jpg')"
-          />
         </div>
-        <span class="footer">ⓒ INU 2019</span>
-      </div>
+        <div class="login__contributors">
+          <span>INU 2019</span>
+          <span>여준호</span>
+          <span>문형일</span>
+          <span>장종우</span>
+          <span>김한경</span>
+          <span>박승아</span>
+        </div>
+      </section>
     </div>
   </div>
 </template>
 
 <style lang="scss" scoped>
-.page {
-  height: auto;
-  margin: 0;
-  background: linear-gradient(to bottom, rgba(0, 0, 0, 0.65), rgba(0, 0, 0, 0.8));
-
-  @media (min-height: 767px) {
-    height: inherit;
-  }
-}
-
-.content {
-  height: -webkit-fill-available;
-  padding-top: 0;
-  padding-bottom: 0;
-}
-
-.card-wrap {
-  height: -webkit-fill-available;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  flex-direction: column;
-
-  @media (max-width: 800px) {
-    height: unset;
-  }
-}
-
-.card {
-  max-width: 65rem;
-  min-height: 40rem;
-  box-shadow: 15px 19px 32px -18px rgba(21, 19, 19, .07);
-  background-color: white;
-  border-radius: 8px;
-  display: flex;
-  flex-direction: row;
-
-  @media (max-width: 800px) {
-    width: 95%;
-    flex-direction: column;
-  }
-
-  @media (max-width: 800px) {
-    margin-top: 2rem;
-    margin-bottom: 2rem;
-  }
-}
-
 .login {
-  width: 50%;
-  padding: 2rem;
+  height: 100%;
+  margin: 0;
 
-  @media (max-width: 800px) {
-    padding: 1.2rem;
-    padding-top: 2rem;
-    width: unset;
+  &__content {
+    height: -webkit-fill-available;
+    padding-top: 0;
+    padding-bottom: 0;
+    height: -webkit-fill-available;
+    display: flex;
+    flex-direction: row;
+
+    @media (max-width: 400px) {
+      flex-direction: column;
+    }
+  }
+
+  &__image-wrap {
+    height: 100%;
+    width: 35%;
+
+    @media (max-width: 630px) {
+      width: 25%;
+    }
+
+    @media (max-width: 400px) {
+      margin-bottom: 2rem;
+    }
+
+    img {
+      height: 100vh;
+      width: 100%;
+      object-fit: cover;
+      object-position: center;
+
+      @media (max-width: 400px) {
+        display: none;
+      }
+    }
+  }
+
+  &__section {
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+
+    &-content {
+      width: 95%;
+      display: flex;
+      justify-content: space-evenly;
+
+      @media (max-width: 1000px) {
+        width: 80%;
+        flex-direction: column;
+      }
+    }
+  }
+
+  &__main {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    width: 40%;
+
+    @media (max-width: 1000px) {
+      width: unset;
+    }
   }
 
   &__title {
     margin: 0;
-    font-size: 2.5rem;
-    display: inline-block;
-    word-break: keep-all;
-    margin-bottom: 1rem;
+    font-size: 5.2rem;
+    letter-spacing: 4px;
+    font-weight: 900;
 
-    @media (max-width: 1000px) {
-      font-size: 2.2rem;
+    @media (max-width: 1150px) {
+      font-size: 4rem;
     }
 
-    @media (max-width: 800px) {
-      font-size: 1.8rem;
+    @media (max-width: 600px) {
+      font-size: 3.5rem;
     }
-  }
 
-  &__brand {
-    color: #ed1280;
+    @media (max-width: 400px) {
+      font-size: 3rem;
+    }
   }
 
   &__desc {
     margin: 0;
-    font-size: 1.1rem;
-    display: inline-block;
+    font-size: 1.2rem;
+    font-weight: 300;
     word-break: keep-all;
 
-    @media (max-width: 800px) {
-      margin-top: 0.5rem;
-      font-size: 0.9rem;
-    }
-  }
-
-  &__notice {
-    list-style: none;
-    padding: 0;
-    font-weight: 300;
-    line-height: 1.5;
-    font-size: 1.1rem;
-
-    li {
-      display: inline-block;
-      word-break: keep-all;
-      border-left: 5px solid lightgray;
-      color: gray;
-      padding-left: 0.5rem;
-      margin-bottom: 0.7rem;
-    }
-
-    li strong {
-      font-weight: 500;
-    }
-
-    @media (max-width: 800px) {
-      font-size: 0.88rem;
-    }
-  }
-
-  &__input {
-    width: 100%;
-    font-size: 1.2rem;
-    padding: 0.8rem;
-    padding-left: 1.2rem;
-    padding-right: 1.2rem;
-    box-sizing: border-box;
-    margin-bottom: 1.2rem;
-    border-radius: 25px;
-    border: 0;
-    background-color: rgb(241, 241, 241);
-    box-shadow: 15px 19px 32px -18px rgba(21, 19, 19, .07);
-
-    &:focus {
-      outline: none;
-    }
-
-    @media (max-width: 800px) {
-      font-size: 1rem;
+    @media (max-width: 630px) {
+      font-size: 1.1rem;
     }
   }
 
   &__button {
-    cursor: pointer;
-    width: 100%;
-    display: flex;
-    justify-content: center;
-    align-items: center;
+    border: none;
+    border-radius: 25px;
     color: white;
     background-color: #ed1280;
     font-size: 1.2rem;
-    padding: 1rem 0;
-    margin-top: 1rem;
-    border-radius: 25px;
-    border: 0;
-    box-shadow: 15px 19px 32px -18px rgba(21, 19, 19, .07);
+    font-weight: 800;
+    padding: 0.8rem 2.4rem;
+    margin-top: 1.2rem;
+    font-family: 'Montserrat', sans-serif;
+    user-select: none;
+    cursor: pointer;
 
-    @media (max-width: 800px) {
-      font-size: 1.1rem;
-      padding: 0.8rem 0;
-      margin-bottom: 1rem;
+    &:active,
+    &:focus {
+      outline: none;
     }
   }
-}
 
-.image {
-  width: 50%;
-  object-fit: cover;
-  border-top-right-radius: 8px;
-  border-bottom-right-radius: 8px;
+  &__cards {
+    display: flex;
+    flex-direction: column;
+    width: 40%;
+    margin-top: 1.5rem;
 
-  @media (max-width: 800px) {
-    width: 100%;
-    height: 15rem;
+    @media (max-width: 1000px) {
+      width: unset;
+    }
   }
-}
 
-.footer {
-  margin-top: 0.5rem;
-  margin-bottom: 0.5rem;
-  font-size: 1.1rem;
-  font-weight: 500;
-  color: white;
+  &__card {
+    height: 4.8rem;
+    width: 100%;
+    margin-bottom: 0.8rem;
+    background-color: white;
+    border-radius: 20px;
+    color: black;
+    padding: 1rem;
+    display: flex;
+    align-items: center;
+    box-shadow: 10px 10px 25px -1px rgba(0, 0, 0, .06);
 
-  @media (max-width: 800px) {
-    margin-top: -1rem;
-    margin-bottom: 1.5rem;
+    @media (max-width: 1000px) {
+      width: unset;
+    }
+
+    @media (max-width: 630px) {
+      padding: 0.6rem;
+    }
+
+    img {
+      height: 4rem;
+      width: 4rem;
+      border-radius: 20px;
+      box-shadow: 10px 10px 25px -1px rgba(0, 0, 0, .06);
+      margin-right: 1.2rem;
+    }
+
+    h1 {
+      margin: 0;
+      margin-bottom: 0.2rem;
+      width: 100%;
+      word-break: keep-all;
+      font-size: 1.3rem;
+
+      @media (max-width: 630px) {
+        font-size: 1.1rem;
+      }
+
+      @media (max-width: 380px) {
+        font-size: 1rem;
+      }
+
+      @media (max-width: 360px) {
+        font-size: 0.9rem;
+      }
+    }
+
+    span {
+      font-weight: bold;
+      color: rgba(0, 0, 0, 0.7);
+
+      &.value {
+        color: #ed1280;
+      }
+    }
+  }
+
+  &__contributors {
+    position: absolute;
+    bottom: 3rem;
+    right: 3rem;
+
+    @media (max-width: 400px) {
+      position: unset;
+      width: 100%;
+      text-align: center;
+      margin: 1rem 0;
+      font-size: 0.8rem;
+    }
+
+    span {
+      font-weight: 300;
+      color: #aaa;
+      margin-left: 0.3rem;
+
+      &:first-child {
+        color: #888;
+      }
+    }
+  }
+
+  &__form {
+
+    &-content {
+      display: flex;
+      flex-direction: column;
+    }
+
+    input {
+      width: 80%;
+      background-color: #fae7f1;
+      border: none;
+      border-radius: 24px;
+      font-size: 1.1rem;
+      padding: 1rem 1.2rem;
+      margin-top: 0.8rem;
+
+      @media (max-width: 400px) {
+        width: unset;
+      }
+
+      &:active,
+      &:focus {
+        outline: none;
+      }
+    }
   }
 }
 </style>
